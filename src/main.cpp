a05b188f9c17c272c0bc7beedad0dffa0f8a5296
Krzysiek1105 <krzysiek1105@opoczta.pl>
2019-03-04 13:43:01 +0100

    main.cpp refactorization

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,23 @@
-#include <stdio.h>
-#include <string>
 #include "chessboard.h"
 #include "piece.h"
-#include <stdlib.h>
-#include <SFML/Graphics.hpp>
-
-#define WINDOW_SIZE 929
-
-void setPieces(Chessboard &chessboard, std::vector<sf::Sprite> &pieces, std::vector<sf::Texture> &textures)
-{
-	int i = 0;
-	for (int x = 0; x < 8; x++)
-	{
-		for (int y = 0; y < 8; y++)
-		{
-			if (chessboard.pieces[x][y].pieceType == EMPTY)
-				continue;
-
-			Piece piece = chessboard.pieces[x][y];
-			pieces[i].setPosition(x * (WINDOW_SIZE / 8), WINDOW_SIZE - y * (WINDOW_SIZE / 8) - 96);
-			int type = piece.pieceType + (piece.side == BLACK ? 6 : 0) - 1;
-			pieces[i].setTexture(textures[type]);
-			i++;
-		}
-	}
-}
+#include "graphics.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE, WINDOW_SIZE), "Chess", sf::Style::Titlebar | sf::Style::Close);
 	window.setFramerateLimit(30);
 
-	sf::Texture whiteSquareTexture;
-	whiteSquareTexture.loadFromFile("img/whiteSquare.png");
-	sf::Texture blackSquareTexture;
-	blackSquareTexture.loadFromFile("img/blackSquare.png");
+	std::vector<sf::Texture> piecesTextures = loadPiecesTextures();
+	std::vector<sf::Texture> boardTextures = loadBoardTextures();
 
-	std::vector<sf::Sprite> squares;
-	for (int y = 7; y >= 0; y--)
-	{
-		for (int x = 0; x < 8; x++)
-		{
-			sf::Sprite tmp;
-			if ((x + y) % 2 == 0)
-				tmp.setTexture(whiteSquareTexture);
-			else
-				tmp.setTexture(blackSquareTexture);
-			tmp.setPosition(x * (WINDOW_SIZE / 8), y * (WINDOW_SIZE / 8));
+	std::vector<sf::Sprite> squares = setCheckerboard(boardTextures);
+	std::vector<sf::Sprite> pieces = setPieces();
 
-			squares.push_back(tmp);
-		}
-	}
+	Chessboard chessboard;
+	int piecesOnBoard = updatePieces(chessboard, pieces, piecesTextures);
 
 	Position from;
 	Position to;
-
-	Chessboard chessboard;
-
-	std::vector<sf::Texture> textures;
-	for (int i = 1; i < 13; i++)
-	{
-		sf::Texture tmp;
-		std::string name = "img/" + std::to_string(i) + ".png";
-		tmp.loadFromFile(name);
-
-		textures.push_back(tmp);
-	}
-
-	std::vector<sf::Sprite> pieces;
-	for (int i = 0; i < 32; i++)
-	{
-		sf::Sprite tmp;
-		pieces.push_back(tmp);
-	}
-
-	setPieces(chessboard, pieces, textures);
-
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -86,10 +28,8 @@ int main()
 
 			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				int x = event.mouseButton.x;
-				int y = event.mouseButton.y;
-				int fieldX = (x / (float)WINDOW_SIZE) * 8;
-				int fieldY = 8 - (y / (float)WINDOW_SIZE) * 8;
+				int fieldX = (event.mouseButton.x / (float)WINDOW_SIZE) * 8;
+				int fieldY = 8 - (event.mouseButton.y / (float)WINDOW_SIZE) * 8;
 
 				from.x = fieldX;
 				from.y = fieldY;
@@ -103,7 +43,7 @@ int main()
 						int n = m.to.x + m.to.y * 8;
 						squares[n].setColor(sf::Color(140, 205, 16, 255));
 					}
-					else if (m.moveType == Chessboard::CASTLING && m.side == side)
+					else if (m.moveType == Chessboard::CASTLING && m.side == side && chessboard.pieces[from.x][from.y].pieceType == KING)
 					{
 						if (m.side == WHITE)
 						{
@@ -177,7 +117,7 @@ int main()
 					chessboard.makeMove(from, to);
 
 				std::cout << chessboard;
-				setPieces(chessboard, pieces, textures);
+				piecesOnBoard = updatePieces(chessboard, pieces, piecesTextures);
 
 				for (int i = 0; i < 64; i++)
 					squares[i].setColor(sf::Color(255, 255, 255, 255));
@@ -192,7 +132,7 @@ int main()
 		window.clear();
 		for (int i = 0; i < 64; i++)
 			window.draw(squares[i]);
-		for (int i = 0; i < 32; i++)
+		for (int i = 0; i < piecesOnBoard; i++)
 			window.draw(pieces[i]);
 		window.display();
 	}
